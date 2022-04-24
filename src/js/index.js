7a2fb6bc41b981e6b7a0f9173f215d4895ad76ba
joyboy10 <csbt1033@gmail.com>
2022-04-24 21:09:08 +0900

    webpack img improve

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,10 @@ import "./components/header";
 
 import "./screen/home";
 // screen
+
+import "../assets/images/slider_1.jpeg";
+import "../assets/images/slider_2.jpeg";
+import "../assets/images/slider_3.jpeg";
+import "../assets/images/slider_4.jpeg";
+import "../assets/images/slider_5.jpeg";
+// images
