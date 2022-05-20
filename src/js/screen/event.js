d874fcb4e78fe082fe7e77d6777fd773247e910b
zoo <csbt1033@gmail.com>
2022-05-20 09:49:09 +0900

    회사에서 받음

--- src/js/screen/event.js
+++ src/js/screen/event.js
@@ -5,9 +5,9 @@ eMenus.forEach(eMenu=>{eMenu.addEventListener("click",menuOn)});
 function menuOn(){
     eMenus.forEach(eMenu=>{eMenu.classList.remove("on")});//on초기화
     eContainer.forEach(box=>{box.style.display="none"});//display초기화
-    
+
     this.classList.add("on"); //선택메뉴 on효과
-    
+
     for( var i = 0 ; i < eMenus.length; i++ ){
         if(eMenus[i].classList.contains("on")){
             eContainer[i].style.display = "block";
@@ -15,7 +15,7 @@ function menuOn(){
             finisheSet();
             announceSet();
         }
-    }//eContainer change효과  
+    }//eContainer change효과
 };
 //종료된 이벤트
 const finishedBoxs = document.querySelectorAll(".finished_box");
@@ -41,7 +41,7 @@ function finishedOn(){
         if(finishedNumBtns[i].classList.contains("on")){
             finishedBoxs[i].style.display = "flex";
         }
-    }//finishedBoxs change효과  
+    }//finishedBoxs change효과
 }
 
 const preBtn = document.querySelector(".pre");
@@ -103,7 +103,7 @@ function announceOn(){
         if(announceBtns[i].classList.contains("on")){
             announceBoxs[i].style.display = "table";
         }
-    }//finishedBoxs change효과  
+    }//finishedBoxs change효과
 }
 
 
